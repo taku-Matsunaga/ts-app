af2478c8bdcf6705fe99f4d158c3b5179a33e55e
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-15 13:58:54 +0900

    first

---
+++ src/router/Router.tsx
@@ -0,0 +1,34 @@
+import { FC, memo } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Login } from "../components/pages/Login";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { HomeRoutes } from "./HomeRoutes";
+
+export const Router: FC = memo(() => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route path="/home" render={({ match: { url } }) => (
+        <Switch>
+          {HomeRoutes.map((route) => (
+            <Route
+              key={route.path}
+              exact={route.exact}
+              path={`${url}${route.path}`}
+            >
+              <HeaderLayout>
+                {route.children}
+              </HeaderLayout>
+            </Route>
+          ))}
+        </Switch>
+      )} />
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  )
+});

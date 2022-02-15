d700961c5703511e0917667dd025298cb9c048fe
Taku Matsunaga <corvettez06511@gmail.com>
2022-02-15 22:54:35 +0900

    list

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,35 @@
-import { FC, memo } from "react";
+/*eslint-disable*/
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { FC, memo, useEffect } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー管理ページです</p>
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
